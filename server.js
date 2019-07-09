747ea2cc35316c6b6763f591c70fe67813f745d8
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-07-09 14:02:37 -0700

    commit "user" changes

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express =  require('express');
 const app = express();
 const mongoose = require('mongoose');
+const users= require('./routes/api/users');
+const profile = require ('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
-app.get('/', (req, res) => res.send('Hello'));
-const port = 7000;
-app.listen(port,()=>console.log(`server is running on port ${port}`));
 
 //Db config
 const db = require ('./config/keys').mongoURI;
 mongoose.connect(db).then(() => console.log('MongoDb connected')).catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send('Hello'));
+app.use('/api/users', users);
+//app.use('/api/profile', profile);
+//app.use('/api/posts', posts);
+
+const port = 7000;
+app.listen(port,()=>console.log(`server is running on port ${port}`));
