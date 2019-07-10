394f343dd3d87304bd756e53170f840ec25f744f
Anna Li <annacyema@gmail.com>
2019-07-09 20:21:00 -0700

    jwt passport/profiles/validation udpate

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const isEmpty = value => 
-    value === undefined ||
-    value === null ||
-    (typeof value === 'object' && Object.keys(value).length === 0) ||
-    (typeof value === 'string' && value.trim().length === 0);
-=======
 // old way: function isEmpty(value){}
 
 //new way
@@ -13,6 +6,5 @@ const isEmpty = value =>
    value === null ||
    (typeof value === 'object' && Object.keys(value).length === 0) ||
    (typeof value === 'string' && value.trim().length === 0);
->>>>>>> 4e4c06544e85138c399597e4f58a7f74f7d264bb
 
 module.exports = isEmpty;
