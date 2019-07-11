5f6fbca6c636b899255e0224eb3a16fd76afb4d8
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-07-11 09:22:26 -0700

    commit

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
+
 const isEmpty = value => 
     value === undefined ||
     value === null ||
     (typeof value === 'object' && Object.keys(value).length === 0) ||
     (typeof value === 'string' && value.trim().length === 0);
-=======
-// old way: function isEmpty(value){}
 
-//new way
-const isEmpty = value =>
-   value === undefined ||
-   value === null ||
-   (typeof value === 'object' && Object.keys(value).length === 0) ||
-   (typeof value === 'string' && value.trim().length === 0);
->>>>>>> 4e4c06544e85138c399597e4f58a7f74f7d264bb
 
 module.exports = isEmpty;
