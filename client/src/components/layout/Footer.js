326274230684eed6cced684317555a555fdfab3f
AdelaSeb <a.sebestova@outlook.com>
2019-07-26 00:00:12 -0700

    added UI for main screen

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,8 +1,8 @@
-import React from './node_modules/react'
+import React, { Component }  from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer className="bg-light text-black mt-5 p-4 text-center">
     Copyright &copy; 2019 InstaPlus
   </footer>
   )
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,30 @@
-import React, { Component } from './node_modules/react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                Navbar
-            </div>
+            <nav className="navbar navbar-expand-sm navbar-light bg-light mb-4">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h1 className="display-3 mb-4">InstaPlus</h1>
+                            <form className="form-inline">
+                                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                                <div className="btn btn-lg btn-dark mr-2">
+                                Sign Up
+                                </div>
+                                <div className="btn btn-lg btn-light">
+                                Login
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         )
     }
-}
+};
 
 export default Navbar;
