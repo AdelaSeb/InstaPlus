6d9008d7132cd37d0713b407de777ba6db1652cb
AdelaSeb <a.sebestova@outlook.com>
2019-07-25 22:58:37 -0700

    created react app and installed libraries

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from './node_modules/react'
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                Landing
+            </div>
+        )
+    }
+}
+
+export default Landing;
