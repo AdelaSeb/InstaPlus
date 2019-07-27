75344f2011d5b3b707e6b8f0e301a9c7e70168e8
AdelaSeb <a.sebestova@outlook.com>
2019-07-27 07:41:34 -0700

    hooked up login with axios

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,23 +7,23 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
                 <div className="container">
-                <Link className="navbar-brand" to="/">InstaPlus</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                    <Link className="navbar-brand" to="/">InstaPlus</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                     <span className="navbar-toggler-icon"></span>
-                </button>
+                    </button>
 
-                <div className="collapse navbar-collapse" id="mobile-nav">
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Sign Up</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+                    <div className="collapse navbar-collapse" id="mobile-nav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">Sign Up</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         )
     }
 };
